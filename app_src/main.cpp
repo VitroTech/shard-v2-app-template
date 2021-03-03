48d625a6aa017a34e6194a7ab1b746d9d52210b8
Marek Kasiewicz <marek.kasiewicz@3mdeb.com>
2021-03-03 01:25:39 +0100

    app_src/main.cpp: Remove unnecessary code
    libs/vitroio-sdk: use commit with updated IotBlock
    
    Signed-off-by: Marek Kasiewicz <marek.kasiewicz@3mdeb.com>

--- app_src/main.cpp
+++ app_src/main.cpp
@@ -1,15 +1,8 @@
 #include <mbed.h>
 #include <shard-sdk.h>
 #include <global_consts.h>
-/**
- * Place for including application dependent drivers and interfaces
- */
-
-
 
 using namespace vitroio::sdk;
-//If your drivers and sensors use namespaces, remember to use them.
-
 
 #define MAIN_MODULE_NAME "MAIN"
 #define MAIN_INFO(format, ...) VITROIO_DEBUG_INFO(MAIN_MODULE_NAME, format, ##__VA_ARGS__);
@@ -17,8 +10,6 @@ using namespace vitroio::sdk;
 #define MAIN_ERROR(format, ...) VITROIO_DEBUG_ERROR(MAIN_MODULE_NAME, format, ##__VA_ARGS__);
 
 #define SERIAL_BAUDRATE 115200
-
-#define TIMESTAMP_REQUEST_PARAMETER_ID 0x40
 
 /**
  * Watchdog
@@ -35,8 +26,6 @@ DigitalOut statusLed(VITROIO_TEMPLATE_STATUS_LED_PIN);
 /**
  * Event queues and threads
  */
-Thread normalPriorityThread(osPriorityNormal, 0x1800);
-EventQueue normalPriorityEventQueue(32*EVENTS_EVENT_SIZE);
 Thread highPriorityThread(osPriorityHigh, 0x1800);
 EventQueue highPriorityEventQueue(32*EVENTS_EVENT_SIZE);
 
@@ -52,7 +41,6 @@ void frameReceivedCallback(const CanbusFrame& frame);
 /**
  * vitroio-sdk objects
  */
-IoTBlock iotBlock;
 Canbus canbus(CAN_RX, CAN_TX, &highPriorityEventQueue);
 
 NodeController node(
@@ -75,40 +63,6 @@ NodeController node(
     &highPriorityEventQueue
 );
 
-//MeasurementApi template parameter is maximum number of registered sensors.
-//If you want to register more then 4 change this number.
-MeasurementApi<4> measurementApi(&normalPriorityEventQueue, 32);
-
-/**
- * global variable for timestamps from Gateway
- */
-volatile uint32_t timestampGlobal = 0;
-
-/**
- * @brief Function called by Canbus when frame is received
- * 
- * @param frame Received frame
- */
-void frameReceivedCallback(const CanbusFrame& frame)
-{
-    if (frame.parameterId() == TIMESTAMP_REQUEST_PARAMETER_ID)
-    {
-        timestampGlobal = *((uint32_t*)frame.data());
-    }
-}
-
-/**
- * Place for sensors drivers declaration and initialisation
- */
-
-
-
-/**
- * Place for sensors interfaces declaration and initialisation
- */
-
-
-
 int main()
 {
     MAIN_INFO("Application started; (id: %d) v%d.%d.%d.%d; vitroio-sdk v%s",
@@ -119,18 +73,10 @@ int main()
     wdtKicker.attach(callback(&wdt, &Watchdog::Service), 2.0);
 
     highPriorityThread.start(callback(&highPriorityEventQueue, &EventQueue::dispatch_forever));
-    normalPriorityThread.start(callback(&normalPriorityEventQueue, &EventQueue::dispatch_forever));
 
-    if(!canbus.isValid() || !node.isValid() || !measurementApi.isValid()){
+    if(!canbus.isValid() || !node.isValid()){
         MAIN_ERROR("Failed to initialize vitroio-sdk objects");
         NVIC_SystemReset();
-    }
-
-    int frameReceivedHandler = 0;
-    frameReceivedHandler = canbus.setOnFrameReceivedCallback(callback(&frameReceivedCallback));
-
-    if(frameReceivedHandler < 0){
-        MAIN_ERROR("Failed to set callback for received frames");
     }
 
     if (shardEdgeConfig.configureShardEdge() != 0){
@@ -142,66 +88,13 @@ int main()
         MAIN_ERROR("Failed to initialize communication");
     }
 
-    /**
-     * Measurement api initialization
-     */
-    measurementApi.setPollTime(10);
-    //Uncomment the line below and provide the sensor interface variable
-    //measurementApi.registerSensor(&/*your_sensor_interface*/);
-    measurementApi.setOnNewValuesCallback(measurementsHandler);
-    measurementApi.startMeasurements();
+    IoTBlock iotBlock = IoTBlock(&canbus, node.nodeId());
 
     while(1) {
-        ThisThread::sleep_for(0xFFFFFFFF);
+        iotBlock.make(0, 0x132);
+        iotBlock.send();
+        ThisThread::sleep_for(5000);
     }
 
     return 0;
-}
-
-/**
- * @brief Function called by measurementApi
- * 
- * @details This function is called with a selected period. An array of sensors
- * values and its size are passed as arguments. The function prepares IoTBlocks
- * containing sensors values and sends them by CAN bus
- * 
- * @param values Pointer to a sensors values array
- * @param quantity Number of sensors values in the array
- */
-void measurementsHandler(SensorParameterValue* values, int quantity)
-{
-    updateTimestamp();
-
-    for(int i=0; i<quantity; i++){
-        iotBlock.make_iot(values[i].value, values[i].parameter, timestampGlobal);
-        iotBlock.CAN_iot(&canbus, node.nodeId());
-        ThisThread::sleep_for(10);
-    }
-}
-
-/**
- * @brief Sends timestamp request to Gateway and waits for response
- * 
- * @note Uses CAN bus to send timestamp request. Canbus object required
- */
-void updateTimestamp()
-{
-    int err = -1;
-    CanbusFrame canFrameTimestamp;
-
-    canFrameTimestamp.setNodeId(node.nodeId());
-    canFrameTimestamp.setParameterId(TIMESTAMP_REQUEST_PARAMETER_ID);
-    memset(canFrameTimestamp.data(), 0x00, 1);
-    canFrameTimestamp.data()[0] = 0x00;
-
-    err = canbus.sendFrame(canFrameTimestamp);
-
-    if(err != VITROIO_ERR_SUCCESS){
-        MAIN_ERROR("Failed to send timestamp request over CAN bus: %d", err);
-    }
-    /**
-     * Give some time for response
-     */
-    ThisThread::sleep_for(10);
-    MAIN_INFO("Timestamp = %lu", (timestampGlobal));
 }

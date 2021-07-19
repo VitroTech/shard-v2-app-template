3625932d7bad543b41624df933d252cb3bf5e8de
ruben crispino <ruben.crispino@hotmail.it>
2021-07-19 14:18:18 +0200

    Improved build_app script

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ] 
+if [ $# -eq 0 ]
 then
     echo "Specify application to build."
     exit 1
@@ -10,12 +10,26 @@ application="Applications/$1"
 echo "$application"
 
 if [ ! -d $application ]
-then 
+then
     echo "No directory named after given application."
     exit 1
 fi
 
 # Do a backup, just in case
-mv app_src/* app_src_backup
-cp -r $application/. app_src/
-./docker_build_image.sh
+#Check if folder is empty
+DIR=app_src_backup
+if [ -d "$DIR" ]
+then
+	if [ "$(ls -A $DIR)" ]; then
+     echo "Take action $DIR is not Empty"
+     DIR_NAME="backup_"$(date +%s)
+     mkdir $DIR/$DIR_NAME
+     mv "$DIR"/* $DIR/$DIR_NAME
+	else
+     mv app_src/* app_src_backup
+     cp -r $application/. app_src/
+     ./docker_build_image.sh
+	fi
+else
+	echo "Directory $DIR not found."
+fi

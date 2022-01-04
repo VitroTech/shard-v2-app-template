2336095e6e611d8ad93570a0b3fb5eaa42f9b2d6
Guilherme Danielski <danielski.guilherme@gmail.com>
2022-01-04 08:59:10 -0300

    Add functions, variables, and checks for docker and files

--- docker_build_image.sh
+++ docker_build_image.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/home/build vitrotech/docker-mbed-cli /bin/bash -c ./build_image.sh
+# Stop the script for errors
+set -o errexit
+set -o pipefail
+
+# Check if the command is supported
+_command_exists() {
+    
+    command -v "$@" > /dev/null 2>&1
+    
+}
+
+# Check if a file exists
+_file_exists(){
+    
+    if [ ! -f ./"$@" ]
+    then
+        echo "File $@ is missing!"
+        echo "Aborting..."
+        exit 1
+    fi
+    
+}
+
+# Run the Docker image
+_run_docker(){
+    
+    _file_exists $build_script
+    
+    if _command_exists docker
+    then
+        docker run --rm -it -v $PWD:/home/build vitrotech/docker-mbed-cli /bin/bash -c ./$build_script
+    else
+        echo "Docker is not installed."
+        echo "Aborting ..."
+        exit 1
+    fi
+    
+}
+
+# Main function
+_main() {
+    
+    build_script="build_image.sh"
+    _run_docker
+    
+}
+
+_main

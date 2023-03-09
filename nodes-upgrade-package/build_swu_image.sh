ecff4f7e6ee45d95c1e532c572e2182ee4d46615
Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>
2023-03-09 13:31:28 +0100

    nodes-upgrade-package: build_swu_image: fix used logic to build swu package
    Signed-off-by: Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>

--- nodes-upgrade-package/build_swu_image.sh
+++ nodes-upgrade-package/build_swu_image.sh
@@ -3,7 +3,7 @@
 # Check for the argument
 _argument_check() {
 
-    if [ ! -d ./../Applications/$1 ]
+    if [ ! -d $SCRIPT_PATH/../Applications/$1 ]
     then
         echo "Specify application to build. It must be located in the Application folder."
         exit 1
@@ -12,10 +12,20 @@ _argument_check() {
 }
 
 PRODUCT_NAME="$1"
+SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+WORK_DIR="$SCRIPT_PATH/bin/$PRODUCT_NAME"
+APP_BUILD_DIR="$SCRIPT_PATH/../bin/$PRODUCT_NAME"
+SW_DESCRIPTION="sw-description"
+SW_DESCRIPTION_TEMPLATE="sw-description-template"
+APP_A_NAME="app_a.bin"
+APP_B_NAME="app_b.bin"
+UPGRADE_PACKAGE_JSON="upgrade_package.json"
+UPGRADE_PACKAGE_JSON_TEMPLATE="upgrade_package_template.json"
+SHELLSCRIPT="trigger_nodes_upgrade.sh"
 
 _argument_check $PRODUCT_NAME
 
-eval $(sed -n 's/^#define  *\([^ ]*\)  *\(.*\) *$/export \1=\2/p' ../common/global_consts.h)
+eval $(sed -n 's/^#define  *\([^ ]*\)  *\(.*\) *$/export \1=\2/p' $SCRIPT_PATH/../common/global_consts.h)
 FIRMWARE_VERSION=($VITROIO_TEMPLATE_VERSION_MAJOR.$VITROIO_TEMPLATE_VERSION_MINOR.$VITROIO_TEMPLATE_VERSION_PATCH.$VITROIO_TEMPLATE_VERSION_RC)
 
 if [ $VITRIOIO_TEMPLATE_FIRMWARE_ID -gt 1023 ]; then
@@ -43,23 +53,12 @@ if [ $VITROIO_TEMPLATE_VERSION_RC -gt 15 ]; then
     exit 1
 fi
 
-WORK_DIR="./bin/$PRODUCT_NAME"
-
-SW_DESCRIPTION="sw-description"
-SW_DESCRIPTION_TEMPLATE="sw-description-template"
-APPS_PATH="../bin/$PRODUCT_NAME"
-APP_A_NAME="app_a.bin"
-APP_B_NAME="app_b.bin"
-UPGRADE_PACKAGE_JSON="upgrade_package.json"
-UPGRADE_PACKAGE_JSON_TEMPLATE="upgrade_package_template.json"
-SHELLSCRIPT="trigger_nodes_upgrade.sh"
-
 echo -e "\nFirmware id is: ${VITRIOIO_TEMPLATE_FIRMWARE_ID}"
 echo -e "\nFirmware version is: ${FIRMWARE_VERSION}\n"
 
 [ -d $WORK_DIR ] || mkdir -p $WORK_DIR
 
-cp $SW_DESCRIPTION_TEMPLATE  "$WORK_DIR/$SW_DESCRIPTION"
+cp "$SCRIPT_PATH/$SW_DESCRIPTION_TEMPLATE" "$WORK_DIR/$SW_DESCRIPTION"
 
 echo "Setting id in ${SW_DESCRIPTION} file..."
 sed -e "s/id = \"x\"/id = \"${VITRIOIO_TEMPLATE_FIRMWARE_ID}\"/" -i "$WORK_DIR/$SW_DESCRIPTION"
@@ -67,7 +66,7 @@ sed -e "s/id = \"x\"/id = \"${VITRIOIO_TEMPLATE_FIRMWARE_ID}\"/" -i "$WORK_DIR/$
 echo "Setting version in ${SW_DESCRIPTION} file..."
 sed -e "s/.*version =.*/\tversion = \"${FIRMWARE_VERSION}\";/" -i "$WORK_DIR/$SW_DESCRIPTION"
 
-cp $UPGRADE_PACKAGE_JSON_TEMPLATE  "$WORK_DIR/$UPGRADE_PACKAGE_JSON"
+cp "$SCRIPT_PATH/$UPGRADE_PACKAGE_JSON_TEMPLATE" "$WORK_DIR/$UPGRADE_PACKAGE_JSON"
 
 echo "Setting id in ${UPGRADE_PACKAGE_JSON} file..."
 sed -e "s/\"id\" : \"x\"/\"id\": \"${VITRIOIO_TEMPLATE_FIRMWARE_ID}\"/" -i "$WORK_DIR/$UPGRADE_PACKAGE_JSON"
@@ -75,16 +74,16 @@ sed -e "s/\"id\" : \"x\"/\"id\": \"${VITRIOIO_TEMPLATE_FIRMWARE_ID}\"/" -i "$WOR
 echo "Setting version in ${UPGRADE_PACKAGE_JSON} file..."
 sed -e "s/.*version.*/    \"version\": \"${FIRMWARE_VERSION}\",/" -i "$WORK_DIR/$UPGRADE_PACKAGE_JSON"
 
-cp "$APPS_PATH/$APP_A_NAME" "$WORK_DIR"
-cp "$APPS_PATH/$APP_B_NAME" "$WORK_DIR"
-cp "$SHELLSCRIPT" "$WORK_DIR"
+cp "$APP_BUILD_DIR/$APP_A_NAME" "$WORK_DIR"
+cp "$APP_BUILD_DIR/$APP_B_NAME" "$WORK_DIR"
+cp "$SCRIPT_PATH/$SHELLSCRIPT" "$WORK_DIR"
 
 cd $WORK_DIR
 echo -e "\nBuilding SWU image...\n"
 FILES="${SW_DESCRIPTION} ${APP_A_NAME} ${APP_B_NAME} ${UPGRADE_PACKAGE_JSON} ${SHELLSCRIPT}"
 for i in $FILES;do
-        echo $i;done | cpio -ov -H crc > ${PRODUCT_NAME}_${FIRMWARE_VERSION}.swu
+        echo "$i";done | cpio -ov -H crc > ${PRODUCT_NAME}_${FIRMWARE_VERSION}.swu
 
 for i in $FILES;do
-        rm $i;
+        rm "$i";
 done
